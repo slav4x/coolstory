22dbee988353e4145870647a17333d191706199e
Slava Khripushin <slava.cpa@yandex.ru>
2024-01-21 19:00:34 +0700

    init

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+/* eslint-disable operator-linebreak */
+/* eslint-disable no-inner-declarations */
+
+const viewportFix = (width) => {
+  const meta = document.querySelector('meta[name="viewport"]');
+  meta.setAttribute('content', `user-scalable=no, width=${screen.width <= width ? width : 'device-width'}`);
+};
+
+viewportFix(420);
