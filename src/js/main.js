70dac291de04d0dca755d3d180cc802e3709aeab
Slava Khripushin <slava.cpa@yandex.ru>
2024-01-22 16:33:15 +0700

    header

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,26 @@ const viewportFix = (width) => {
 };
 
 viewportFix(420);
+
+function toggleShow(element, target) {
+  element.addEventListener('click', (event) => {
+    event.stopPropagation();
+    target.classList.toggle('show');
+  });
+
+  document.addEventListener('click', (event) => {
+    const isClickInsideTarget = target.contains(event.target);
+
+    if (!isClickInsideTarget && target.classList.contains('show')) {
+      target.classList.remove('show');
+    }
+  });
+}
+
+const burgerIcon = document.querySelector('.header-burger');
+const burgerNav = document.querySelector('.header-nav');
+toggleShow(burgerIcon, burgerNav);
+
+const cityActive = document.querySelector('.header-city a');
+const cityList = document.querySelector('.header-city__popup');
+toggleShow(cityActive, cityList);
