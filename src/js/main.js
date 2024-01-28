cf2b09c5b37e562bd2446ee6ae9412e0d6f30eee
Slava Khripushin <slava.cpa@yandex.ru>
2024-01-28 19:52:43 +0700

    blog single

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,19 @@ const viewportFix = (width) => {
 viewportFix(420);
 
 document.addEventListener('DOMContentLoaded', function () {
+  const maskOptions = {
+    mask: '+7 (000) 000-00-00',
+    onFocus: function () {
+      if (this.value === '') this.value = '+7 ';
+    },
+    onBlur: function () {
+      if (this.value === '+7 ') this.value = '';
+    },
+  };
+
+  const maskedElements = document.querySelectorAll('.masked');
+  maskedElements.forEach((item) => new IMask(item, maskOptions));
+
   function toggleShow(element, target) {
     element.addEventListener('click', (event) => {
       event.stopPropagation();
